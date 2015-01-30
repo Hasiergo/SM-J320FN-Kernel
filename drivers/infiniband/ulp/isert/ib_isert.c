81a4fcd175229106a16fd11c90d4d339973bd049
Sagi Grimberg <sagig@mellanox.com>
2015-01-30 22:17:26 +0000

    iser-target: Fix flush + disconnect completion handling
    commit 128e9cc84566a84146baea2335b3824288eed817 upstream.
    
    ISER_CONN_UP state is not sufficient to know if
    we should wait for completion of flush errors and
    disconnected_handler event.
    
    Instead, split it to 2 states:
    - ISER_CONN_UP: Got to CM connected phase, This state
    indicates that we need to wait for a CM disconnect
    event before going to teardown.
    
    - ISER_CONN_FULL_FEATURE: Got to full feature phase
    after we posted login response, This state indicates
    that we posted recv buffers and we need to wait for
    flush completions before going to teardown.
    
    Also avoid deffering disconnected handler to a work,
    and handle it within disconnected handler.
    More work here is needed to handle DEVICE_REMOVAL event
    correctly (cleanup all resources).
    
    Squashed:
    
    iser-target: Don't deffer disconnected handler to a work
    
    Signed-off-by: Sagi Grimberg <sagig@mellanox.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/infiniband/ulp/isert/ib_isert.c
+++ drivers/infiniband/ulp/isert/ib_isert.c
@@ -574,6 +574,7 @@ isert_connected_handler(struct rdma_cm_id *cma_id)
 
 	pr_info("conn %p\n", isert_conn);
 
+<<<<<<< HEAD
 	if (!kref_get_unless_zero(&isert_conn->conn_kref)) {
 		pr_warn("conn %p connect_release is running\n", isert_conn);
 		return;
@@ -583,6 +584,10 @@ isert_connected_handler(struct rdma_cm_id *cma_id)
 	if (isert_conn->state != ISER_CONN_FULL_FEATURE)
 		isert_conn->state = ISER_CONN_UP;
 	mutex_unlock(&isert_conn->conn_mutex);
+=======
+	isert_conn->state = ISER_CONN_UP;
+	kref_get(&isert_conn->conn_kref);
+>>>>>>> dc0672f1f216... iser-target: Fix flush + disconnect completion handling
 }
 
 static void
@@ -609,6 +614,7 @@ isert_put_conn(struct isert_conn *isert_conn)
  *
  * Notes:
 <<<<<<< HEAD
+<<<<<<< HEAD
  * In case the connection state is FULL_FEATURE, move state
  * to TEMINATING and start teardown sequence (rdma_disconnect).
  * In case the connection state is UP, complete flush as well.
@@ -616,6 +622,11 @@ isert_put_conn(struct isert_conn *isert_conn)
  * In case the connection state is UP, move state
  * to TEMINATING and start teardown sequence (rdma_disconnect).
 >>>>>>> 839eac57ebae... iscsi,iser-target: Initiate termination only once
+=======
+ * In case the connection state is FULL_FEATURE, move state
+ * to TEMINATING and start teardown sequence (rdma_disconnect).
+ * In case the connection state is UP, complete flush as well.
+>>>>>>> dc0672f1f216... iser-target: Fix flush + disconnect completion handling
  *
  * This routine must be called with conn_mutex held. Thus it is
  * safe to call multiple times.
@@ -627,17 +638,32 @@ isert_conn_terminate(struct isert_conn *isert_conn)
 {
 	int err;
 
-	if (isert_conn->state == ISER_CONN_UP) {
-		isert_conn->state = ISER_CONN_TERMINATING;
+	switch (isert_conn->state) {
+	case ISER_CONN_TERMINATING:
+		break;
+	case ISER_CONN_UP:
+		/*
+		 * No flush completions will occur as we didn't
+		 * get to ISER_CONN_FULL_FEATURE yet, complete
+		 * to allow teardown progress.
+		 */
+		complete(&isert_conn->conn_wait_comp_err);
+	case ISER_CONN_FULL_FEATURE: /* FALLTHRU */
 		pr_info("Terminating conn %p state %d\n",
 			   isert_conn, isert_conn->state);
+		isert_conn->state = ISER_CONN_TERMINATING;
 		err = rdma_disconnect(isert_conn->conn_cm_id);
 		if (err)
 			pr_warn("Failed rdma_disconnect isert_conn %p\n",
 				   isert_conn);
+		break;
+	default:
+		pr_warn("conn %p teminating in state %d\n",
+			   isert_conn, isert_conn->state);
 	}
 }
 
+<<<<<<< HEAD
 >>>>>>> 839eac57ebae... iscsi,iser-target: Initiate termination only once
 static void
 isert_conn_terminate(struct isert_conn *isert_conn)
@@ -709,6 +735,8 @@ isert_disconnected_handler(struct rdma_cm_id *cma_id,
 	complete(&isert_conn->conn_wait);
 }
 
+=======
+>>>>>>> dc0672f1f216... iser-target: Fix flush + disconnect completion handling
 static int
 isert_disconnected_handler(struct rdma_cm_id *cma_id)
 >>>>>>> 839eac57ebae... iscsi,iser-target: Initiate termination only once
@@ -731,6 +759,7 @@ isert_disconnected_handler(struct rdma_cm_id *cma_id)
 	complete(&isert_conn->conn_wait);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 	if (terminating)
 		goto out;
 =======
@@ -745,6 +774,14 @@ isert_disconnected_handler(struct rdma_cm_id *cma_id)
 		queue_work(isert_release_wq, &isert_conn->release_work);
 	}
 	mutex_unlock(&isert_np->np_accept_mutex);
+=======
+	mutex_lock(&isert_conn->conn_mutex);
+	isert_conn_terminate(isert_conn);
+	mutex_unlock(&isert_conn->conn_mutex);
+
+	pr_info("conn %p completing conn_wait\n", isert_conn);
+	complete(&isert_conn->conn_wait);
+>>>>>>> dc0672f1f216... iser-target: Fix flush + disconnect completion handling
 
 out:
 	return 0;
@@ -1008,9 +1045,13 @@ isert_put_login_tx(struct iscsi_conn *conn, struct iscsi_login *login,
 				return ret;
 
 			/* Now we are in FULL_FEATURE phase */
+<<<<<<< HEAD
 			mutex_lock(&isert_conn->conn_mutex);
 			isert_conn->state = ISER_CONN_FULL_FEATURE;
 			mutex_unlock(&isert_conn->conn_mutex);
+=======
+			isert_conn->state = ISER_CONN_FULL_FEATURE;
+>>>>>>> dc0672f1f216... iser-target: Fix flush + disconnect completion handling
 			goto post_send;
 		}
 
