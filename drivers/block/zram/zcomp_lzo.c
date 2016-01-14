a1abad81a4fc850ed60587cff85ebd72c29c1f3b
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2016-01-14 15:22:35 -0800

    UPSTREAM: zram/zcomp: do not zero out zcomp private pages
    (cherry picked from commit e02d238c9852a91b30da9ea32ce36d1416cdc683)
    
    Do not __GFP_ZERO allocated zcomp ->private pages.  We keep allocated
    streams around and use them for read/write requests, so we supply a
    zeroed out ->private to compression algorithm as a scratch buffer only
    once -- the first time we use that stream.  For the rest of IO requests
    served by this stream ->private usually contains some temporarily data
    from the previous requests.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/zram/zcomp_lzo.c
+++ drivers/block/zram/zcomp_lzo.c
@@ -10,27 +10,28 @@
 #include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/lzo.h>
+#include <linux/vmalloc.h>
+#include <linux/mm.h>
 
 #include "zcomp_lzo.h"
 
-static void *lzo_create(void)
+static void *lzo_create(gfp_t flags)
 {
-	return kzalloc(LZO1X_MEM_COMPRESS, GFP_KERNEL);
+	void *ret;
+
+	ret = kmalloc(LZO1X_MEM_COMPRESS, flags);
+	if (!ret)
+		ret = __vmalloc(LZO1X_MEM_COMPRESS,
+				flags | __GFP_HIGHMEM,
+				PAGE_KERNEL);
+	return ret;
 }
 
 static void lzo_destroy(void *private)
 {
-	kfree(private);
+	kvfree(private);
 }
 
-<<<<<<< HEAD
-static int lzo_flags(void)
-{
-	return 0;
-}
-
-=======
->>>>>>> 426b56b75428... Rebase zram and zsmalloc from 3.15.
 static int lzo_compress(const unsigned char *src, unsigned char *dst,
 		size_t *dst_len, void *private)
 {
@@ -39,11 +40,7 @@ static int lzo_compress(const unsigned char *src, unsigned char *dst,
 }
 
 static int lzo_decompress(const unsigned char *src, size_t src_len,
-<<<<<<< HEAD
-		unsigned char *dst, void *private)
-=======
 		unsigned char *dst)
->>>>>>> 426b56b75428... Rebase zram and zsmalloc from 3.15.
 {
 	size_t dst_len = PAGE_SIZE;
 	int ret = lzo1x_decompress_safe(src, src_len, dst, &dst_len);
@@ -55,9 +52,5 @@ struct zcomp_backend zcomp_lzo = {
 	.decompress = lzo_decompress,
 	.create = lzo_create,
 	.destroy = lzo_destroy,
-<<<<<<< HEAD
-	.flags = lzo_flags,
-=======
->>>>>>> 426b56b75428... Rebase zram and zsmalloc from 3.15.
 	.name = "lzo",
 };
