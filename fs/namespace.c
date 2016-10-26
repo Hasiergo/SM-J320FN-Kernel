b625399626e56ee78127ba476361a4fca17a9f07
Daniel Rosenberg <drosen@google.com>
2016-10-26 15:58:22 -0700

    vfs: Allow filesystems to access their private mount data
    Now we pass the vfsmount when mounting and remounting.
    This allows the filesystem to actually set up the mount
    specific data, although we can't quite do anything with
    it yet. show_options is expanded to include data that
    lives with the mount.
    
    To avoid changing existing filesystems, these have
    been added as new vfs functions.
    
    Change-Id: If80670bfad9f287abb8ac22457e1b034c9697097
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -787,7 +787,7 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 	if (flags & MS_KERNMOUNT)
 		mnt->mnt.mnt_flags = MNT_INTERNAL;
 
-	root = mount_fs(type, flags, name, data);
+	root = mount_fs(type, flags, name, &mnt->mnt, data);
 	if (IS_ERR(root)) {
 		free_vfsmnt(mnt);
 		return ERR_CAST(root);
@@ -1852,8 +1852,14 @@ static int do_remount(struct path *path, int flags, int mnt_flags,
 		err = change_mount_flags(path->mnt, flags);
 	else if (!capable(CAP_SYS_ADMIN))
 		err = -EPERM;
-	else
-		err = do_remount_sb(sb, flags, data, 0);
+	else {
+		err = do_remount_sb2(path->mnt, sb, flags, data, 0);
+		namespace_lock();
+		br_write_lock(&vfsmount_lock);
+		propagate_remount(mnt);
+		br_write_unlock(&vfsmount_lock);
+		namespace_unlock();
+	}
 	if (!err) {
 		br_write_lock(&vfsmount_lock);
 		mnt_flags |= mnt->mnt.mnt_flags & ~MNT_USER_SETTABLE_MASK;
