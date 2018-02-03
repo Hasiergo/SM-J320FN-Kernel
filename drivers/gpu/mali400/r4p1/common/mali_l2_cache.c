varunchitre15/thunderzap_sprout

mali: increase l2 cache max read size
Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

drivers/misc/mediatek/gpu/mt6582/mali/mali/common/mali_l2_cache.c
@@ -65,7 +65,7 @@ typedef enum mali_l2_cache_status {
 	MALI400_L2_CACHE_STATUS_DATA_BUSY    = 0x02, /**< L2 cache is busy handling data requests */
 } mali_l2_cache_status;
 
-#define MALI400_L2_MAX_READS_DEFAULT 0x1C
+#define MALI400_L2_MAX_READS_DEFAULT 0x30
 
 static struct mali_l2_cache_core *mali_global_l2_cache_cores[MALI_MAX_NUMBER_OF_L2_CACHE_CORES] = { NULL, };
 static u32 mali_global_num_l2_cache_cores = 0;

Hasiergo/SM-J320FN-Kernel

Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

drivers/gpu/mali400/r4p1/common/mali_l2_cache.c
@@ -65,7 +65,7 @@ typedef enum mali_l2_cache_status {
 	MALI400_L2_CACHE_STATUS_DATA_BUSY    = 0x02,
 } mali_l2_cache_status;
 
-#define MALI400_L2_MAX_READS_NOT_SET -1
+#define MALI400_L2_MAX_READS_NOT_SET 0x30
 
 static struct mali_l2_cache_core *
 		mali_global_l2s[MALI_MAX_NUMBER_OF_L2_CACHE_CORES] = { NULL, };